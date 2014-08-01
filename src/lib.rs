ff98bd1f81f0f50a72857dd454d714da9cd58003
hirschen <falco.hirschenberger@itwm.fraunhofer.de>
2014-08-01 16:44:24 +0200

    Add digital out functions

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ extern crate serialize;
 use std::io::timer::sleep;
 use std::iter::range_inclusive;
 use std::fmt::{Show, Formatter, FormatError};
+use std::default::Default;
 use usb::libusb;
 
 
@@ -18,6 +19,7 @@ bitflags!(
 
 bitflags!(
   flags DigitalChannel: u8 { 
+    static Zero = 0,
     static D1 = 1,
     static D2 = 2,
     static D3 = 4,
@@ -25,7 +27,8 @@ bitflags!(
     static D5 = 16,
     static D6 = 32,
     static D7 = 64,
-    static D8 = 128
+    static D8 = 128,
+    static All = 255
   }
 )
 
@@ -48,10 +51,17 @@ enum Packet {
     Status(u8, u8, u8, u8)
 }
 
+#[deriving(Default)]
+struct State {
+    dig: u8,
+    ana1: u8,
+    ana2: u8
+}
 
 pub struct K8055 {
     dev: usb::Device,
-    hd: Option<usb::DeviceHandle>
+    hd: Option<usb::DeviceHandle>,
+    state: State    
 }
 
 impl K8055 {
@@ -68,7 +78,7 @@ impl K8055 {
         };
         
         if d.is_some() { 
-            return Some(K8055{ dev: d.unwrap(), hd: None }) 
+            return Some(K8055{ dev: d.unwrap(), hd: None, state: Default::default() }) 
         } else {
             return None
         }
@@ -86,6 +96,29 @@ impl K8055 {
       }
     }
     
+    pub fn reset(&mut self) -> bool {
+        self.write(&SetAnalogDigital(0u8, 0u8, 0u8))
+    }
+
+    pub fn write_digital_out(&mut self, d: DigitalChannel) -> bool {
+        let p = &SetAnalogDigital(d.bits, self.state.ana1, self.state.ana2);
+        self.write(p)
+    }
+
+    pub fn write_digital_out_mask(&mut self, d: DigitalChannel, mask: DigitalChannel) -> bool {
+        self.write_digital_out(d & mask)
+    }
+
+    pub fn get_digital_out(&mut self) -> DigitalChannel {
+        DigitalChannel::from_bits(self.state.dig).unwrap()
+    }
+   
+    pub fn get_digital_out_mask(&mut self, d: DigitalChannel) -> DigitalChannel {
+        DigitalChannel::from_bits(self.state.dig).unwrap() & d
+    }
+
+// private 
+
     fn find_any_k8055(c: &usb::Context) -> Option<usb::Device> {
         for pid in range_inclusive(Card1.bits, Card4.bits) {
           let d = c.find_by_vid_pid(VendorId, pid);
@@ -104,7 +137,16 @@ impl K8055 {
               };
               
               match hd.write(0x1, libusb::LIBUSB_TRANSFER_TYPE_INTERRUPT, data) {
-                  Ok(_) => return true,
+                  Ok(_) => {
+                      // update the internal state on output changes
+                      match *p {
+                          SetAnalogDigital(d, a1, a2) => {
+                              self.state = State{dig: d, ana1: a1, ana2: a2};
+                          }
+                          _ => ()
+                      }
+                      return true
+                  }
                   Err(_) => return false
               }
           }
@@ -166,26 +208,29 @@ fn find_and_open() {
   assert!(k.is_some());
   let mut k = k.unwrap();
   assert!(k.open());
-  for i in range(0u8, 255) {
-    assert!(k.write(&SetAnalogDigital(i, i, 255 - i)));
-    sleep(10);
-  }
-
-  assert!(k.write(&SetAnalogDigital(0u8, 0u8, 0u8)));
-
-
-  for i in range(0i, 100) {
-    match k.read() {
-        Some(Status(dig, _, _, _)) => assert_eq!(dig, 0u8),
-        _                          => fail!("Err")
-
-    }
-    sleep(10);
-  }
-
   assert!(K8055::new_addr(Card2).is_none());
   assert!(K8055::new_addr(Card3).is_none());
   assert!(K8055::new_addr(Card4).is_none());
+}
+
+#[test()]
+fn write_and_read_digital() {
+  let k = K8055::new();
+  assert!(k.is_some());
+  let mut k = k.unwrap();
+  assert!(k.open());
+  assert!(k.get_digital_out() == Zero);
+  for i in range(0u, 8) {
+    assert!(k.write_digital_out(DigitalChannel::from_bits(1u8<<i).unwrap()));
+    assert!(k.get_digital_out() == DigitalChannel::from_bits(1u8<<i).unwrap());
+    sleep(100);
+  }
+  assert!(k.reset());
+  assert!(k.get_digital_out() == Zero);
+
+  assert!(k.write_digital_out_mask(D1 | D2 | D3, D2));
+  assert!(k.get_digital_out() == D2);
+  assert!(k.reset());
 }
          
 
